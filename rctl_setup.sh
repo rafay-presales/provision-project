0e6fe4f5d27412a82569b76c97588d86164decb7
Kurt Madel <kurt.madel@rafay.co>
2023-06-21 17:35:47 -0400

    Delete rctl_setup.sh

--- rctl_setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-curl -sL https://s3-us-west-2.amazonaws.com/rafay-prod-cli/publish/rctl-linux-amd64.tar.bz2 > rctl.tar.bz2
-tar xvf ./rctl.tar.bz2
-chmod 755 ./rctl
-rm ./rctl.tar.bz2
-sudo cp rctl /usr/local/bin/.
-rctl
