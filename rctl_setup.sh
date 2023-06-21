2ceaefd90ae471d4bcf65c26197062bb5392cc93
Kurt Madel <kurt.madel@rafay.co>
2023-06-21 13:00:40 -0400

    Update rctl_setup.sh

--- rctl_setup.sh
+++ rctl_setup.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-mkdir -p bin
-curl -O https://s3-us-west-2.amazonaws.com/rafay-prod-cli/publish/rctl-linux-amd64.tar.bz2 ./bin
-tar xvf ./bin/rctl-linux-amd64.tar.bz2
-chmod 755 ./bin/rctl
-rm ./bin/rctl-linux-amd64.tar.bz2
+curl -O https://s3-us-west-2.amazonaws.com/rafay-prod-cli/publish/rctl-linux-amd64.tar.bz2 .
+tar xvf ./rctl-linux-amd64.tar.bz2
+chmod 755 ./rctl
+rm ./rctl-linux-amd64.tar.bz2
